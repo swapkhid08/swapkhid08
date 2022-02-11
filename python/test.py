fd0768e4938bc33c066890075f8427b95ebc24c9
swapkhid08 <swapnil.khidake@itspe.co.in>
2022-02-11 18:47:49 +0530

    changes

---
+++ python/test.py
@@ -0,0 +1,41 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist.append("pineapple")
+# thislist.insert(1,"jackfruit")
+# for x in thislist:
+#     print(x)
+# [print(x) for x in thislist]
+
+# newlist = []
+
+# for x in thislist:
+#   if "a" in x:
+#     newlist.append(x)
+
+# print(newlist)
+# print(thislist)
+
+# thislist.sort(reverse = True)
+# print(thislist)
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+
+# list3 = list1 + list2
+# print(list3)
+
+# thistuple = ("apple", "banana", "cherry")
+# thistuple = tuple(("apple", "banana", "cherry"))
+# print(len(thistuple))
+# print(type(thistuple))
+
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[2:5])
+# print(thistuple[1:3])
+
+x = ("apple", "banana", "cherry")
+print (x)
+y = list(x)
+
+y.append("jackfruit")
+print(y)
